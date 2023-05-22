d4b3b29c43c1e73c019ec1efbd213a6c5a8fa673
stephanie181201 <107057565+stephanie181201@users.noreply.github.com>
2023-05-22 00:15:07 -0600

    Add files via upload

---
+++ potencia1/RedesSociales/src/test/java/com/estetica/RedesSociales/RedesSocialesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estetica.RedesSociales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedesSocialesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
